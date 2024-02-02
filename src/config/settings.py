0ebedfdb66a994b5e7fc885dcc369dddc67f0d2b
Mohammad Amin <mohammadaminmiri22@gmail.com>
2024-02-02 18:43:05 +0330

    Debugged settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -19,9 +19,8 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-
 # Loading environment variables
-load_dotenv(BASE_DIR)
+load_dotenv(BASE_DIR/".env")
 
 
 # Quick-start development settings - unsuitable for production
