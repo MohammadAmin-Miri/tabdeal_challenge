b37ae2932763d9bde20250859043bc3ec88cc079
Mohammad Amin <mohammadaminmiri22@gmail.com>
2024-02-02 19:38:42 +0330

    added admin panel for Wallet and Transaction models

--- src/financial/admin.py
+++ src/financial/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from financial.models import Transaction, Wallet
+
+
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "credit")
+
+
+@admin.register(Transaction)
+class WalletAdmin(admin.ModelAdmin):
+    list_filter = (
+        "action",
+        "result",
+    )
+    list_display = (
+        "id",
+        "wallet",
+        "tracking_code",
+        "amount",
+        "action",
+        "result",
+    )
