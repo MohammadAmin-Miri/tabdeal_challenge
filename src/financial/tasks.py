736173b824711aac523929a1b67ca9e5949db86d
Mohammad Amin <mohammadaminmiri22@gmail.com>
2024-02-02 23:40:24 +0330

    debugged increase credit task

--- src/financial/tasks.py
+++ src/financial/tasks.py
@@ -8,6 +8,8 @@ from financial.models import Transaction, Wallet
 @transaction.atomic
 def create_transaction(transaction_id: int):
     transaction = Transaction.objects.get(id=transaction_id)
+    transaction.status = Transaction.StatusChoices.SUCCESS
+    transaction.save()
     wallet = transaction.wallet
     wallet.credit += transaction.amount
     wallet.save()
