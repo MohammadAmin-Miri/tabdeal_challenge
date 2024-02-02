9c7b62401312eb9bde2c3e9135adc2ce897b8bb6
Mohammad Amin <mohammadaminmiri22@gmail.com>
2024-02-02 19:28:10 +0330

    Refactored settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -20,7 +20,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Loading environment variables
-load_dotenv(BASE_DIR/".env")
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     "drf_spectacular_sidecar",
     # Local
     "user",
-    "financial"
+    "financial",
 ]
 
 MIDDLEWARE = [
