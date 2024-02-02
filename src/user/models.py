d180ab75cb9d8bd67cf0483a01806257f920407e
Mohammad Amin <mohammadaminmiri22@gmail.com>
2024-02-02 20:20:24 +0330

    Added Seller and PhoneCharge model and ran makemigrations

--- src/user/models.py
+++ src/user/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from financial.models import Transaction
+
+
+user_model = get_user_model()
+
+
+class Seller(models.Model):
+
+    user = models.OneToOneField(
+        user_model, on_delete=models.CASCADE, related_name="seller"
+    )
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.Index(
+                fields=[
+                    "user",
+                ]
+            ),
+        ]
+
+
+class PhoneCharge(models.Model):
+
+    class StatusChoices(models.TextChoices):
+        SUCCESS = "success", _("Success")
+        FAILURE = "failure", _("Failure")
+        PENDING = "pending", _("Pending")
+
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE, related_name="charges")
+    transaction = models.OneToOneField(
+        Transaction, on_delete=models.CASCADE, related_name="charge"
+    )
+    amount = models.PositiveBigIntegerField(default=0)
+    phone = models.CharField(max_length=15)
+    status = models.CharField(
+        max_length=16, choices=StatusChoices, default=StatusChoices.PENDING
+    )
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.Index(
+                fields=[
+                    "seller",
+                ]
+            ),
+            models.Index(
+                fields=[
+                    "phone",
+                ]
+            ),
+        ]
